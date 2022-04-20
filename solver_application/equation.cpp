d717c29c0e05c08eaf6495ba8f8a3cddee6248a5
Artoym Mokrov <92476117+AstorBrom@users.noreply.github.com>
2022-04-21 01:41:30 +0300

    Update equation.cpp

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -1,30 +1,9 @@
+#include "solver.h"
+#include "formatter_ex.h"
 #include <iostream>
 
-#include "formatter_ex.h"
-#include "solver.h"
-
-int main()
+int main ()
 {
-    float a = 0;
-    float b = 0;
-    float c = 0;
-
-    std::cin >> a >> b >> c;
-
-    float x1 = 0;
-    float x2 = 0;
-
-    try
-    {
-        solve(a, b, c, x1, x2);
-
-        formatter(std::cout, "x1 = " + std::to_string(x1));
-        formatter(std::cout, "x2 = " + std::to_string(x2));
-    }
-    catch (const std::logic_error& ex)
-    {
-        formatter(std::cout, ex.what());
-    }
-
+    print(count5(10, 10));
     return 0;
 }
